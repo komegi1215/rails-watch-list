c8d83a2d23148f8665c08b69761e245749a438a8
Megumi Hirao <komegi.blog@gmail.com>
2024-01-13 15:16:50 +0900

    Updated bookmark

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
+
   validates :name, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: [:new, :create, :index, :show]
-
+  resources :lists, only: [:new, :create, :index, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
