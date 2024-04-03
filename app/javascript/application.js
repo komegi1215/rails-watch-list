eb7d19e0813603c6554645a77bebbb1bf41735f3
Megumi Hirao <komegi.blog@gmail.com>
2024-04-03 23:14:46 +0900

    UPdated JS to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,28 @@ import "@popperjs/core"
 import "./controllers/bootstrap_confirm"
 
 //= require rails-ujs
+
+import { Modal } from 'bootstrap'
+
+const bootstrapConfirm = (message) => {
+  const modalElement = document.getElementById('turbo-confirm-modal')
+  const messageElement = document.getElementById('turbo-confirm-modal-message')
+  const confirmButtonElement = document.getElementById('turbo-confirm-modal-confirm-button')
+  const modal = new Modal(modalElement)
+
+  messageElement.textContent = message
+  modal.show()
+
+  return new Promise((resolve) => {
+    confirmButtonElement.addEventListener(
+      'click',
+      () => {
+        resolve(true)
+        modal.hide()
+      },
+      { once: true },
+    )
+  })
+}
+
+Turbo.setConfirmMethod(bootstrapConfirm)
